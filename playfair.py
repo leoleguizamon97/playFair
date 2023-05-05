d6e91d522f512da1a586750b49ee0a1f6aab3f52
leoleguizamon <leoleguizamon97@outlook.com>
2023-05-05 10:19:42 -0500

    Funcion limpiarllave

--- playfair.py
+++ playfair.py
@@ -1,1 +1,15 @@
 #playfair
+
+llave=input()
+cadena=input()
+
+alfabeto=['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+def limpiarLlave(llave):
+	llave = llave.lower()
+	llaveLimpia= []
+	for c in llave:
+		if not (c in llaveLimpia): llaveLimpia.append(c)
+
+limpiarLlave(llave)
