0522b74f2297942a5bdf5a98728e1ae90a39960e
leoleguizamon <leoleguizamon97@outlook.com>
2023-05-05 13:13:41 -0500

    limpiar cadena implementado

--- playfair.py
+++ playfair.py
@@ -1,11 +1,17 @@
 #playfair
+import os
 
-llave=input()
-cadena=input()
-
+#Variables
 alfabeto=['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-def limpiarLlave(llave,alfabeto):
+#Funciones
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def limpiarLlave(llave,alfabeto):#Limpia la llave
 	llave = llave.lower()
 	llaveLimpia= []
 	for c in llave:
@@ -30,6 +36,52 @@ def generarmatriz(llave,alfabeto):#Genera la matriz de encriptaccion
 				contador += 1
 			else: 
 				matriz[i][j] = alfabeto.pop(0)
+	#Retorna la matriz de encriptacion
 	return matriz
 
-print(generarmatriz(limpiarLlave(llave,alfabeto),alfabeto))
+def limpiarCadena(cadena,alfabeto):
+	cadenaLimpia=[]
+	cadena = cadena.replace('j','i')
+	listaCadena = []
+	for c in cadena:
+		if c in alfabeto:
+			listaCadena.append(c)
+	#Genera las parejas
+	while True:
+		
+		if len(listaCadena) == 0: break
+		if len(listaCadena) == 1: listaCadena.append('x')
+
+		if listaCadena[0] == listaCadena[1]:
+			#Si es repetido
+			c1 = listaCadena.pop(0)
+			cadenaLimpia.append([c1,'x'])
+		else:
+			c1 = listaCadena.pop(0)
+			c2 = listaCadena.pop(0)
+			cadenaLimpia.append([c1,c2])
+		
+	return cadenaLimpia
+
+def encriptarPlayFair():
+	cadenaEncriptada = ''
+	
+	return cadenaEncriptada
+#MAIN
+clear()
+llave=input('Ingrese llave:\n')
+cadena=input('Ingrese cadena:\n')
+llaveLimpia	= limpiarLlave(llave,alfabeto)
+matriz		= generarmatriz(llaveLimpia, list(alfabeto))
+cadenaLimpia= limpiarCadena(cadena,alfabeto)
+
+#Print
+clear()
+print(llaveLimpia)
+print()
+print(cadenaLimpia)
+print()
+for a in matriz: print(a)
+print()
+print(llave)
+print(cadena)
